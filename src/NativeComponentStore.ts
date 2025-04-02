9d7bfc5f30fe57c5c2f18b2a9a5538dc8c346666
konik <konik.joshi@NLPULTP0507278.local>
2025-04-02 17:44:31 +0530

    Fixed TypeScript config issues

--- src/NativeComponentStore.ts
+++ src/NativeComponentStore.ts
@@ -3,6 +3,8 @@ import { TurboModuleRegistry } from 'react-native';
 
 export interface Spec extends TurboModule {
   multiply(a: number, b: number): number;
+  Button(): void;
+  Text(): void;
 }
 
 export default TurboModuleRegistry.getEnforcing<Spec>('ComponentStore');
--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,8 @@
 import ComponentStore from './NativeComponentStore';
+import { Button, Text } from './components';
 
 export function multiply(a: number, b: number): number {
   return ComponentStore.multiply(a, b);
 }
+
+export { Button, Text };
